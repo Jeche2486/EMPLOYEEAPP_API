4c6a6bb456fec3c21c45a9413a3e6f339d778b27
Jeche2486 <carocalinecherutich@gmail.com>
2024-11-08 11:29:55 +0300

    API

---
+++ app.py
@@ -0,0 +1,11 @@
+#there are two ways of creating REST apis in flask
+#1. using flask without any external library
+#2. using flask_restful 
+
+#we will use the #2
+
+
+#flask_restful can be installed via the pip command:
+#pip3 install flask-restful
+
+from flask import *
